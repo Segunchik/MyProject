7028efd03b674cd311d78243fc44eb302493438f
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2025-08-18 19:36:52 +0300

    creating masking functions

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+# from src.masks import get_mask_account
+print(get_mask_card_number(1234567891234567))
+print(get_mask_account(5964532486745))
---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает номер карты в виде числа и возврвщвет маску карты в виде XXXX XX** **** XXXX"""
+    # Преобразуем номер в строку
+    card_number_str = str(card_number)
+    mask_cardnumber = ''.join(card_number_str[:4] + " " + card_number_str[4:6] + "** ****"+ " " + card_number_str[12:])
+    return mask_cardnumber
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция принимает номер счета и возвращает маску в виде ** ХХХХ"""
+
+    account_number_str = str(account_number)
+
+    return ''.join("**" + account_number_str[-4:])
+
+
+if __name__ != "main.py":
+    print(get_mask_card_number(1234567891234567))
+    print(get_mask_account(12345678945613))
