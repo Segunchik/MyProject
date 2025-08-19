bc8921259029638eb468f716a5c60b10a7cd65a6
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2025-08-19 20:45:36 +0300

    creating file widget and function get_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -6,8 +8,6 @@ def mask_account_card(account_card: str) -> str:
     Маскирует номер карты или счета в зависимости от типа
     """
     parts_of_account = account_card.split()
- #   parts_of_account[-1] = '324685311'
-    print(parts_of_account)
 
     if parts_of_account[0].lower() == "счет":
         parts_of_account[-1] = get_mask_account(int(parts_of_account[-1]))
@@ -16,13 +16,17 @@ def mask_account_card(account_card: str) -> str:
         parts_of_account[-1] = get_mask_card_number(int(parts_of_account[-1]))
         return ' '.join(parts_of_account)
 
-#        print(parts_of_account[-1])
- #   number_acc = ''.join(filter(str.isdigit, account_card)
- #   print(number_acc))
+
+def get_date(date_string: str) -> str:
+    """
+    Преобразует строку с датой из формата "2024-03-11T02:26:18.671407" в формат ДД.ММ.ГГГГ.
+    """
+
+    dt_obj = datetime.fromisoformat(date_string)
+    return dt_obj.strftime("%d.%m.%Y")
 
 
-    return "number_acc"
 
-
-print(mask_account_card("Счет 73654108430135874305"))
-print(mask_account_card("Visa Platinum 7000792289606361"))
+#print(mask_account_card("Счет 73654108430135874305"))
+#print(mask_account_card("Visa Platinum 7000792289606361"))
+#print(get_date("2024-03-11T02:26:18.671407"))
