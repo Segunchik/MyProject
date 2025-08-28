1adb95602cb5e681b1678317fc32c55085be5bb0
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2025-08-28 20:04:07 +0300

    creating function sort_by_date processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,8 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
 
     return [item for item in data if item.get('state') == state]
 
+def sort_by_date(data_list: list, reverse = True) -> list:
+    """
+        Сортирует список словарей по дате в поле 'date'.
+    """
+    return sorted(data_list, key=lambda x: x['date'], reverse=reverse)
