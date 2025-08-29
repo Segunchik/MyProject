4508ae813c7cbbda955f1882a1add4f8bed85cf8
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2025-08-29 18:37:15 +0300

    lintered

--- src/processing.py
+++ src/processing.py
@@ -4,10 +4,11 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
 
     """
 
-    return [item for item in data if item.get('state') == state]
+    return [item for item in data if item.get("state") == state]
 
-def sort_by_date(data_list: list, reverse = True) -> list:
+
+def sort_by_date(data_list: list, reverse=True) -> list:
     """
-        Сортирует список словарей по дате в поле 'date'.
+    Сортирует список словарей по дате в поле 'date'.
     """
-    return sorted(data_list, key=lambda x: x['date'], reverse=reverse)
+    return sorted(data_list, key=lambda x: x["date"], reverse=reverse)
