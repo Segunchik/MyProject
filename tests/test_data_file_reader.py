a7faf184e27ad676ee825aad7a71f044ee5b40d1
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2025-10-17 20:05:16 +0300

    Проверено линтерами

--- tests/test_data_file_reader.py
+++ tests/test_data_file_reader.py
@@ -1,15 +1,16 @@
-import pandas as pd
-import pytest
 from unittest.mock import patch
+
+import pandas as pd
+
 from src.data_file_reader import csv_reader, excel_reader
 
 
 @patch("pandas.read_csv")
 def test_csv_reader(mock_read_csv):
-    mock_read_csv.return_value.to_dict.return_value = [{"amount": 100},{"amount1": 1000}]
+    mock_read_csv.return_value.to_dict.return_value = [{"amount": 100}, {"amount1": 1000}]
     result = csv_reader("test.csv")
-    assert result == [{"amount": 100},{"amount1": 1000}]
-    mock_read_csv.assert_called_once_with("test.csv",delimiter=';')
+    assert result == [{"amount": 100}, {"amount1": 1000}]
+    mock_read_csv.assert_called_once_with("test.csv", delimiter=";")
 
 
 def test_csv_reader_file_not_found():
@@ -20,20 +21,13 @@ def test_csv_reader_file_not_found():
 
 
 def test_excel_reader_success():
-    excel_data = pd.DataFrame({
-        "name": ["Анна", "Борис"],
-        "age": [25, 30],
-        "city": ["Москва", "Санкт-Петербург"]
-    })
+    excel_data = pd.DataFrame({"name": ["Анна", "Борис"], "age": [25, 30], "city": ["Москва", "Санкт-Петербург"]})
 
     with patch("pandas.read_excel") as mock_read_excel:
         mock_read_excel.return_value = excel_data
         result = excel_reader("test.xlsx")
 
-    expected = [
-        {"name": "Анна", "age": 25, "city": "Москва"},
-        {"name": "Борис", "age": 30, "city": "Санкт-Петербург"}
-    ]
+    expected = [{"name": "Анна", "age": 25, "city": "Москва"}, {"name": "Борис", "age": 30, "city": "Санкт-Петербург"}]
 
     assert result == expected
 
